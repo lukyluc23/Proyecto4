f4dceb1b4ed1a9ae0851b7ad8aa683c43b1cd4da
lucas <luky.luc.23@gmail.com>
2024-04-07 19:11:29 -0300

    created auth and some one controllers

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { TokenDecoded } from "../types";
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.json(
+        {
+          message: 'AUTH_REQUIRED'
+        }
+      )
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+      return res.json(
+        {
+          message: 'AUTH_REQUIRED'
+        }
+      )
+    }
+
+    const secret = process.env.JWT_SECRET as string
+    const tokenDecoded = jwt.verify(token, secret) as TokenDecoded
+
+    req.tokenData = tokenDecoded
+    
+    next()
+  } catch (error) {
+    return res.json({
+      error: "Not auth"
+    })
+  }
+}
+
+export { auth }
--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import userRoutes from "./users.routes"
 import appointmentsRoutes from "./appointments.routes"
+import authRoutes from "./auth.routes";
 
 const router = express.Router()
 
 router.use("/users", userRoutes)
 router.use("/appointments", appointmentsRoutes)
+router.use("/auth", authRoutes);
 
 
 export default router
